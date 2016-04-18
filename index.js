a186bdf9d640d4b35e75c9669efa2549d41f39e4
Marcus Souza Fernandes <marcus.fernandes9@gmail.com>
2016-04-17 21:55:23 -0400

    implementação das rotas e endpoints add/delete users

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
-const PORT = 3000;
+import consign from "consign";
+
 const app = express();
-app.get("/", (req, res) => res.json({status: "API - Encurtar Url"}));
-app.listen(PORT, () => console.log('API - Encurtar Url - porta :'+ PORT));
+
+consign()
+	.include("libs/config.js")
+	.then("db.js")
+	.then("libs/middlewares.js")
+	.then("routes")
+	.then("libs/boot.js")
+	.into(app);
